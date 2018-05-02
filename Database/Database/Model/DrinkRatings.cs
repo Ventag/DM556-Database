1cd2e813801127cf7dd3a2226f57e7c47c79df20
Simon Karing <simonkaring@gmail.com>
2018-05-02 14:16:23 +0200

    Added data contracts for the table objects.

--- Database/Database/Model/DrinkRatings.cs
+++ Database/Database/Model/DrinkRatings.cs
@@ -1,8 +1,29 @@
 ﻿using System;
+using System.Runtime.Serialization;
+
 namespace Database.Model
 {
+    [DataContract]
     public class DrinkRatings
     {
+        [DataMember(Name = "userId")]
+        public string UserId { get; set; }
+
+        [DataMember(Name = "drinkId")]
+        public Guid DrinkId { get; set; }
+
+        [DataMember(Name = "rating")]
+        public int Rating { get; set; }
+
+        [DataMember(Name = "comment")]
+        public string Comment { get; set; }
+
+        [DataMember(Name = "helpfull")]
+        public int Helpfull { get; set; }
+
+        [DataMember(Name = "unhelpfull")]
+        public int Unhelpfull { get; set; }
+
         public DrinkRatings()
         {
         }
