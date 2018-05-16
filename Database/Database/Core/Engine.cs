70b3475bc1c3b888c4fc4ce95d6749aaa9ef26b9
Simon Karing <simonkaring@gmail.com>
2018-05-16 16:04:48 +0200

    Revert "ConsoleFormat 0.1"
    This reverts commit 1c65331e050358bc945811467165aab73541a30e.

--- Database/Database/Core/Engine.cs
+++ Database/Database/Core/Engine.cs
@@ -177,23 +177,21 @@ namespace Database.Core
                         if (type)
                         {
                             var drink = (BsonSerializer.Deserialize<DrinkInfo>(document));
-                            Console.WriteLine();
-                            Console.WriteLine("User:        " + drink.UserId);
-                            Console.WriteLine("Gin:         " + drink.Gin);
-                            Console.WriteLine("Tonic:       " + drink.Tonic);
-                            Console.WriteLine("Garnish:     " + drink.Garnish);
-                            Console.WriteLine("Description: " + drink.Description);
+                            Console.WriteLine(drink.UserId);
+                            Console.WriteLine(drink.Gin);
+                            Console.WriteLine(drink.Tonic);
+                            Console.WriteLine(drink.Garnish);
+                            Console.WriteLine(drink.Description);
                         }
                         else
                         {
                             var rating = (BsonSerializer.Deserialize<RatingInfo>(document));
-                            Console.WriteLine();
-                            Console.WriteLine("User:      " + rating.UserId);
-                            Console.WriteLine("DrinkID:   " + rating.DrinkId);
-                            Console.WriteLine("Rating:    " + rating.Rating);
-                            Console.WriteLine("Comment:   " + rating.Comment);
-                            Console.WriteLine("Helpful:   " + rating.Helpfull);
-                            Console.WriteLine("Unhelpful: " + rating.Unhelpfull);
+                            Console.WriteLine("user_id:   " + rating.UserId);
+                            Console.WriteLine("drink_id:  " + rating.DrinkId);
+                            Console.WriteLine("rating:    " + rating.Rating);
+                            Console.WriteLine("comment:   " + rating.Comment);
+                            Console.WriteLine("helpful:   " + rating.Helpfull);
+                            Console.WriteLine("unhelpful: " + rating.Unhelpfull);
                         }
                     }
                 }
@@ -208,12 +206,11 @@ namespace Database.Core
                     foreach(var obj in objects)
                     {
                         var drink = (DrinkInfo)obj;
-                        Console.WriteLine();
-                        Console.WriteLine("User:        " + drink.UserId);
-                        Console.WriteLine("Gin:         " + drink.Gin);
-                        Console.WriteLine("Tonic:       " + drink.Tonic);
-                        Console.WriteLine("Garnish:     " + drink.Garnish);
-                        Console.WriteLine("Description: " + drink.Description);
+                        Console.WriteLine(drink.UserId);
+                        Console.WriteLine(drink.Gin);
+                        Console.WriteLine(drink.Tonic);
+                        Console.WriteLine(drink.Garnish);
+                        Console.WriteLine(drink.Description);
                     }
                     break;
 
@@ -221,12 +218,11 @@ namespace Database.Core
                     foreach (var obj in objects)
                     {
                         var rating = (RatingInfo)obj;
-                        Console.WriteLine();
-                        Console.WriteLine("User:      " + rating.UserId);
-                        Console.WriteLine("DrinkID:   " + rating.DrinkId);
-                        Console.WriteLine("Comment:   " + rating.Comment);
-                        Console.WriteLine("Helpful:   " + rating.Helpfull);
-                        Console.WriteLine("Unhelpful: " + rating.Unhelpfull);
+                        Console.WriteLine(rating.UserId);
+                        Console.WriteLine(rating.DrinkId);
+                        Console.WriteLine(rating.Comment);
+                        Console.WriteLine(rating.Helpfull);
+                        Console.WriteLine(rating.Unhelpfull);
                     }
                     break;
 
@@ -234,8 +230,7 @@ namespace Database.Core
                     foreach (var obj in objects)
                     {
                         var user = (UserInfo)obj;
-                        Console.WriteLine();
-                        Console.WriteLine("User: " + user.Id);
+                        Console.WriteLine(user.Id);
                     }
                     break;
 
